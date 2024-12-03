76bd71480feccbbd6aa41836dd5d11a0f5abe465
brucebett <bett6448@gmail.com>
2024-12-03 22:19:44 +0300

    teu5 commit

--- app/views.py
+++ app/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from app.models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def drink_list(request):
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({"drinks": serializer.data}, safe=False)
 
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({"drinks": serializer.data}, safe=False)
 
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
